3091c67946439752cf98c2f161afa81c086670c2
omesser <odedmesser@gmail.com>
2019-09-19 00:50:33 +0300

    Forcing v2 signature client in constructor

--- s3/s3.go
+++ s3/s3.go
@@ -60,7 +60,8 @@ func NewS3Client(opts S3ClientOpts) (S3Client, error) {
 			minioClient, err = minio.NewWithRegion(
 				s3cli.Endpoint, s3cli.AccessKey, s3cli.SecretKey, s3cli.Secure, s3cli.Region)
 		} else {
-			minioClient, err = minio.New(s3cli.Endpoint, s3cli.AccessKey, s3cli.SecretKey, s3cli.Secure)
+			log.Infof("Creating igz-overridden minio client with V2 signature", s3cli.Endpoint)
+			minioClient, err = minio.NewV2(s3cli.Endpoint, s3cli.AccessKey, s3cli.SecretKey, s3cli.Secure)
 		}
 	} else {
 		log.Infof("Creating minio client %s using IAM role", s3cli.Endpoint)
